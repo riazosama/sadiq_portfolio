3887598800b6e3804fe6f22c47f3e428af70985d
riazosama <riazosama@gmail.com>
2020-05-11 22:40:45 +0500

    Removed unused imports.

--- src/Shared/Portfolio.js
+++ src/Shared/Portfolio.js
@@ -1,17 +1,6 @@
 import React from "react";
 import { withStyles } from "@material-ui/styles";
 import sizes from "../styles/sizes";
-import BallRacer from "../assets/images/ball_racer_1.png";
-import BucketBall from "../assets/images/bucket_ball.png";
-import CleanRoad from "../assets/images/clean_road.png";
-import Cricket from "../assets/images/Cricket.png";
-import Fidget from "../assets/images/fidget.io.png";
-import Flippy from "../assets/images/flippy.png";
-import Jet from "../assets/images/jet.io.png";
-import PolySphere from "../assets/images/poly.png";
-import StackFlow from "../assets/images/stackflow.png";
-import SweetSmash from "../assets/images/sweetsmash.png";
-import TubeRusher from "../assets/images/tuberusher.png";
 import PortfolioItem from "./PortfolioItem";
 import Config from "../config";
 
